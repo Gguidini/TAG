3358ec7f2a8b1db1c66ad64a0326b7b6ccf27059
Gguidini <giovanni.guidini@gmail.com>
2018-03-31 14:08:20 -0300

    Cliques found. Project complete

--- Project1/README.md
+++ Project1/README.md
@@ -1,2 +1,19 @@
+Project 1 was created by Vitor F Dullens and Giovanni M Guidini for our Theory and Application of Graphs discipline at UnB (2018/1).
+
+Special thanks for Thiago Veras Machado for letting us use his gmlParser.h, which was slightly modified by us.
+
 # To do
-- [ ] O projeto consiste em escrever um programa em C/C++ que lê o arquivo (karate.gml), monta com esses dados um grafo não direcionado, sem pesos, usando listas de adjacências, e imprime como saída (tela) em ordem decrescente, os integrantes (vértices) com maiores graus no grafo, até o de menor. Essa impressão deve ser somente pelos integrantes, e consequentes graus desses vértices. Uma visualização em gráfico, mostrando o grafo montado é um ponto adicional (i.e. 11, caso alguém queira fazer). Além disso, o programa deve encontrar e imprimir um (1) clique maximal (com no mínimo 5 alunos), e um (1) clique máximo desse grafo, ambos diferentes, (pelos nomes de todos).
+- [X] O projeto consiste em escrever um programa em C/C++ que lê o arquivo (karate.gml), monta com esses dados um grafo não direcionado, sem pesos, usando listas de adjacências, e imprime como saída (tela) em ordem decrescente, os integrantes (vértices) com maiores graus no grafo, até o de menor. Essa impressão deve ser somente pelos integrantes, e consequentes graus desses vértices. Uma visualização em gráfico, mostrando o grafo montado é um ponto adicional (i.e. 11, caso alguém queira fazer). Além disso, o programa deve encontrar e imprimir um (1) clique maximal (com no mínimo 5 alunos), e um (1) clique máximo desse grafo, ambos diferentes.
+
+# Usage
+    1. Download Project 1
+    2. Compile karate.cpp (we recommend using g++ -std=c++11 -o k)
+    3. Execute informing .gml file name (i.e. ./k karate.gml)
+    
+# Notes
+The file "k" is a compiled version of the program. Should work on linux systems.
+
+File "out_34.html" is a view of the graph.
+
+More cliques are found by the program, but only the 2 bigger ones are shown. To show all cliques
+uncomment showAllCliques() function and recompile.
--- Project1/karate.cpp
+++ Project1/karate.cpp
@@ -1,3 +1,15 @@
+// Study case of W. W. Zachary's Karate Club (1977)
+// For UnB - Theory and Application of Graphs, 2018/1
+// by Giovanni M Guidini 16/0122660 and Vitor F Dullens  16/0148260
+
+// This program explores graph representations and cliques in a graph in the following steps:
+// 1. The graph is generated from a .gml graph using gmlParser.h, by Thiago Veras (modified)
+// 2. A list of nodes is presented, ordered by the decreasing number of connections.
+// 3. A view of the graph is generated using svg.h, by us
+// 4. K-cliques (with k >= 4) are found using Bron-Kerbosch's algorithm with pivoting
+// 5. 2 cliques are presented, being the maximum and a maximal
+
+// test compiled in 4.14.30-1-MANJARO using g++ (GCC) 7.3.1 20180312
 #include <bits/stdc++.h>
 #include "gmlParser.h"
 #include "svg.h"
@@ -27,8 +39,8 @@ bool cmp(const pair<int, int>&i, const pair<int, int>&j){
     return i.second > j.second;
 }
 
-// lista nodes e numero de coonexoes em ordem decrescente
-void listSorted(vector<int>* G){
+// lista nodes e numero de conexoes em ordem decrescente
+vector<pair<int,int>> listSorted(vector<int>* G){
     int l = graphSize();
     // vetor para o indice e numero de conexoes
     vector<pair<int,int>> arr(l);
@@ -44,6 +56,98 @@ void listSorted(vector<int>* G){
     printf("Node\tNumero de Conexoes\n");
     for(int i = 0; i < l; i++){
         cout << getID(arr[i].first) << "\t" << arr[i].second << endl;
+    }
+
+    return arr;
+}
+
+
+vector<int> intersection(vector<int> A, vector<int> B){
+    // necessary to use intersection
+    vector<int> R;
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    set_intersection(A.begin(), A.end(), B.begin(), B.end(), back_inserter(R));
+    return R;
+}
+
+vector<int> difference(vector<int> A, vector<int> B){
+    vector<int> R;
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    set_difference(A.begin(), A.end(), B.begin(), B.end(), back_inserter(R));
+    return R;
+}
+
+void eraseByValue(vector<int>& A, int el){
+    for(int i = 0; i < (int) A.size(); i++){
+        if(A[i] == el){
+            A.erase(A.begin()+i);
+            return;
+        }
+    }
+}
+// global vector to store all the cliques
+vector<vector<int>> cliques;
+// encontra cliques maximais
+void BronKer(vector<int> R, vector<int> P, vector<int> X){
+    // the original graph for reference
+    vector<int>* G = getGraph();
+
+    // if it's a maximal clique, report
+    if(P.empty() && X.empty()){
+        // filter for k-cliques with k >= 4
+        if(R.size() >= 4){
+            cliques.push_back(R);
+        }
+        return;
+    }
+    else if(P.empty()){
+        // no more nodes, but not a clique
+        return;
+    }
+
+    // keeping track of active nodes
+    vector<int> K;
+    
+    // pivot element.
+    int u = P.front();
+    K = difference(P, G[u]);
+
+    // iterate through all possibilities
+    // NOT GETTING THE RIGHT ONES
+    for(auto v : K){
+            R.push_back(v);
+
+            // recursive call
+            BronKer(R, intersection(P, G[v]), intersection(X, G[v]));
+
+            R.pop_back(); // removes v
+            eraseByValue(P, v);
+            X.push_back(v);
+
+    }
+}
+
+bool sortCliques(const vector<int> A, const vector<int> B){
+    return A.size() > B.size();
+}
+
+void join(vector<int> A, char delim = ','){
+    printf("{%d", A[0]);
+    int n = A.size();
+    for(int i = 1; i < n; i++){
+        printf("%c %d", delim, A[i]);
+    }
+    printf("}\n");
+}
+
+// show remaining cliques
+void showAllCliques(){
+    int k = cliques.size();
+    for(int i = 2; i < k; i++){
+        printf("Clique maximal\n");
+        join(cliques[i]);
     }
 }
 
@@ -71,7 +175,32 @@ int main(int argc, char* argv[]){
     vector<int>* G = getGraph();
     
     // sorts nodes by connections
-    listSorted(G);
+    vector<pair<int, int>> sorted_G = listSorted(G);
+
     // generates view of graph
     printGraph(G);
+
+    // how we're only interested in k-cliques with k >= 4
+    // all nodes with less than 4 connections are irrelevant
+    vector<int> bigG, a, b;
+    for(auto e : sorted_G){
+        if(e.second >= 4){
+            bigG.push_back(e.first);
+        }
+    }
+
+    // finds cliques
+    BronKer(a, bigG, b);
+    
+    // sort cliques
+    sort(cliques.begin(), cliques.end(), sortCliques);
+
+    // show two bigger cliques
+    printf("Clique máximo\n");
+    join(cliques[0], ',');
+    printf("Clique maximal\n");
+    join(cliques[1], ',');
+    
+    // Uncomment to list all k-cliques with k >= 4
+    //showAllCliques();
 }
